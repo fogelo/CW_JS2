c6ee943ce1636880d8aab18ce22f56edd8dea4ce
antonorlov <fogelo@yandex.ru>
2022-01-31 21:22:40 +0300

    Convert number to reversed array of digits

--- kata-8kyu.js
+++ kata-8kyu.js
@@ -1,8 +1,16 @@
 /*
-?1.Abbreviate a Two Word Name
+?1. Abbreviate a Two Word Name
 */
 
 function abbrevName(name) {
     let x = name.toUpperCase().split(' ')
     return x[0][0] + '.' + x[1][0]
+}
+
+/*
+?2. Convert number to reversed array of digits
+*/
+
+function digitize(n) {
+    return [...n.toString()].map(item=>Number(item)).reverse()
 }
