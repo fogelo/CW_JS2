bf546674fc33ffff8b3dc523cf9c1bddcd43febf
antonorlov <fogelo@yandex.ru>
2022-02-01 13:15:59 +0300

    Regex count lowercase letters

--- kata-8kyu.js
+++ kata-8kyu.js
@@ -20,12 +20,14 @@
 */
 
 //@ my
+/*
 function basicOp(operation, value1, value2) {
     return operation === '+' ? value1 + value2
         : operation === '-' ? value1 - value2
             : operation === '*' ? value1 * value2
                 : operation === '/' ? value1 / value2 : ''
 }
+*/
 
 //@ best №1
 /*function basicOp(operation, value1, value2) {
@@ -42,10 +44,10 @@ function basicOp(operation, value1, value2) {
             return 0;
     }
 }*/
-//@ best №2
+/*//@ best №2
 function basicOp(o, a, b) {
     return eval(a+o+b);
-}
+}*/
 
 
 //@ best №3
@@ -59,5 +61,22 @@ function basicOp(o, a, b) {
     };
     return cases[operation]
 }*/
+/*
 const result = basicOp('+', 4, 7)
+console.log(result)*/
+
+/*
+?4. Regex count lowercase letters
+Your task is simply to count the total number of lowercase letters in a string.
+*/
+
+function lowercaseCount(str) {
+    return (str.match(/[a-z]/g) || []).length
+}
+
+const result = lowercaseCount('abcABC123')
 console.log(result)
+
+/*
+?5. String cleaning
+*/
